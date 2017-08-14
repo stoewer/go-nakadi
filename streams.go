4cacd5c7856fd9f3c12258b86e9e38be92e02d11
A. Stoewer <adrian.stoewer@rz.ifi.lmu.de>
2017-08-14 22:36:51 +0200

    Implement stream with retry

--- streams.go
+++ streams.go
@@ -3,7 +3,19 @@
 
 package nakadi
 
-import "context"
+import (
+	"context"
+	"time"
+
+	"github.com/cenkalti/backoff"
+)
+
+var defaultStreamOptions = StreamOptions{
+	InitialRetryInterval: time.Millisecond * 50,
+	MaxRetryInterval:     time.Minute,
+	StreamMaxElapsedTime: time.Minute * 5,
+	CommitMaxElapsedTime: time.Minute * 2,
+}
 
 type Cursor struct {
 	Partition      string `json:"partition"`
@@ -19,19 +31,165 @@ type StreamAPI interface {
 	Close() error
 }
 
-type StreamOptions struct{}
+type StreamOptions struct {
+	InitialRetryInterval time.Duration
+	MaxRetryInterval     time.Duration
+	StreamMaxElapsedTime time.Duration
+	CommitMaxElapsedTime time.Duration
+	NotifyErr            func(error, time.Duration)
+	NotifyOK             func()
+}
 
 func NewStream(client *Client, subscriptionID string, options *StreamOptions) StreamAPI {
+	var copyOptions StreamOptions
+	if options != nil {
+		copyOptions = *options
+		if copyOptions.InitialRetryInterval == 0 {
+			copyOptions.InitialRetryInterval = defaultStreamOptions.InitialRetryInterval
+		}
+		if copyOptions.MaxRetryInterval == 0 {
+			copyOptions.MaxRetryInterval = defaultStreamOptions.MaxRetryInterval
+		}
+		if copyOptions.StreamMaxElapsedTime == 0 {
+			copyOptions.StreamMaxElapsedTime = defaultStreamOptions.StreamMaxElapsedTime
+		}
+		if copyOptions.CommitMaxElapsedTime == 0 {
+			copyOptions.CommitMaxElapsedTime = defaultStreamOptions.CommitMaxElapsedTime
+		}
+		if copyOptions.NotifyErr == nil {
+			copyOptions.NotifyErr = func(_ error, _ time.Duration) {}
+		}
+		if copyOptions.NotifyOK == nil {
+			copyOptions.NotifyOK = func() {}
+		}
+	} else {
+		copyOptions = defaultStreamOptions
+	}
+
 	ctx, cancel := context.WithCancel(context.Background())
+
+	streamBackOff := backoff.NewExponentialBackOff()
+	streamBackOff.InitialInterval = copyOptions.InitialRetryInterval
+	streamBackOff.MaxInterval = copyOptions.MaxRetryInterval
+	streamBackOff.MaxElapsedTime = copyOptions.StreamMaxElapsedTime
+
+	commitBackOff := backoff.NewExponentialBackOff()
+	commitBackOff.InitialInterval = copyOptions.InitialRetryInterval
+	commitBackOff.MaxInterval = copyOptions.MaxRetryInterval
+	commitBackOff.MaxElapsedTime = copyOptions.StreamMaxElapsedTime
+
 	s := &httpStreamAPI{
 		opener: &simpleStreamOpener{
 			client:         client,
 			subscriptionID: subscriptionID},
-		eventCh: make(chan eventsOrError, 10),
-		ctx:     ctx,
-		cancel:  cancel}
+		committer: &simpleCommitter{
+			client:         client,
+			subscriptionID: subscriptionID},
+		eventCh:       make(chan eventsOrError, 10),
+		ctx:           ctx,
+		cancel:        cancel,
+		streamBackOff: backoff.WithContext(streamBackOff, ctx),
+		commitBackOff: backoff.WithContext(commitBackOff, ctx),
+		notifyErr:     copyOptions.NotifyErr,
+		notifyOK:      copyOptions.NotifyOK,
+	}
+
 	go s.startStream()
+
 	return s
+}
+
+type httpStreamAPI struct {
+	opener        streamOpener
+	committer     committer
+	eventCh       chan eventsOrError
+	ctx           context.Context
+	cancel        context.CancelFunc
+	commitBackOff backoff.BackOff
+	streamBackOff backoff.BackOff
+	notifyErr     func(error, time.Duration)
+	notifyOK      func()
+}
+
+func (s *httpStreamAPI) NextEvents() (Cursor, []byte, error) {
+	select {
+	case <-s.ctx.Done():
+		return Cursor{}, nil, context.Canceled
+	case next := <-s.eventCh:
+		return next.cursor, next.events, next.err
+	}
+}
+
+func (s *httpStreamAPI) CommitCursor(cursor Cursor) error {
+	var err error
+
+	backoff.RetryNotify(func() error {
+		err = s.committer.commitCursor(cursor)
+		return err
+	}, s.commitBackOff, s.notifyErr)
+
+	if err == nil {
+		s.notifyOK()
+	}
+
+	return err
+}
+
+func (s *httpStreamAPI) Close() error {
+	s.cancel()
+	return nil
+}
+
+func (s *httpStreamAPI) startStream() {
+	for {
+		var err error
+		var stream streamer
+
+		backoff.RetryNotify(func() error {
+			stream, err = s.opener.openStream()
+			return err
+		}, s.streamBackOff, s.notifyErr)
+
+		if err != nil {
+			continue
+		}
+		s.notifyOK()
+
+		var cursor Cursor
+		var events []byte
+		for {
+			// read the next events or close and return
+			select {
+			case <-s.ctx.Done():
+				err = context.Canceled
+			default:
+				cursor, events, err = stream.nextEvents()
+			}
+
+			// write next result (events or error) to channel or close and return
+			select {
+			case <-s.ctx.Done():
+				err = context.Canceled
+			case s.eventCh <- eventsOrError{cursor: cursor, events: events, err: err}:
+				// nothing
+			}
+
+			// in case of errors, close the stream and open a new one or abort when canceled
+			if err != nil {
+				if err == context.Canceled {
+					stream.closeStream()
+					return
+				}
+				break
+			}
+		}
+
+		stream.closeStream()
+	}
+}
+
+type streamOpener interface {
+	openStream() (streamer, error)
 }
 
 type streamer interface {
@@ -43,37 +201,8 @@ type committer interface {
 	commitCursor(cursor Cursor) error
 }
 
-type streamOpener interface {
-	openStream() (streamer, error)
-}
-
 type eventsOrError struct {
 	cursor Cursor
 	events []byte
 	err    error
-}
-
-type httpStreamAPI struct {
-	opener    streamOpener
-	committer committer
-	eventCh   chan eventsOrError
-	ctx       context.Context
-	cancel    context.CancelFunc
-}
-
-func (rs *httpStreamAPI) NextEvents() (Cursor, []byte, error) {
-	return Cursor{}, nil, nil
-}
-
-func (rs *httpStreamAPI) CommitCursor(cursor Cursor) error {
-	return nil
-}
-
-func (rs *httpStreamAPI) Close() error {
-	rs.cancel()
-	return nil
-}
-
-func (rs *httpStreamAPI) startStream() {
-
 }
--- streams_test.go
+++ streams_test.go
@@ -2,3 +2,279 @@
 // All rights reserved.
 
 package nakadi
+
+import (
+	"context"
+	"testing"
+
+	"time"
+
+	"github.com/cenkalti/backoff"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHttpStreamAPI_startStream(t *testing.T) {
+	retryCh := make(chan error, 1)
+	okCh := make(chan struct{}, 1)
+
+	stream := &mockStreamer{}
+	_, opener, _, _ := setupMockStream(retryCh, okCh)
+
+	opener.On("openStream").Once().Return(nil, assert.AnError)
+	opener.unblock <- struct{}{}
+	time.Sleep(50 * time.Millisecond)
+
+	select {
+	case err := <-retryCh:
+		assert.EqualError(t, assert.AnError, err.Error())
+	default:
+		assert.Fail(t, "no retry error detected")
+	}
+
+	opener.On("openStream").Once().Return(stream, nil)
+	opener.unblock <- struct{}{}
+	time.Sleep(50 * time.Millisecond)
+
+	select {
+	case <-okCh:
+		// nothing
+	default:
+		assert.Fail(t, "no retry ok detected")
+	}
+}
+
+func TestHttpStreamAPI_NextEvents(t *testing.T) {
+	expectedCursor := Cursor{NakadiStreamID: "stream-id"}
+	expectedEvents := []byte(`"events":[{"metadata":{"eid":"74450ab6-5461-11e7-9dd2-87c3afa8811f"})]`)
+
+	t.Run("fail with error", func(t *testing.T) {
+		stream := &mockStreamer{unblock: make(chan struct{}, 1)}
+		streamAPI, opener, _, _ := setupMockStream(nil, nil)
+
+		opener.On("openStream").Once().Return(stream, nil)
+		opener.unblock <- struct{}{}
+		stream.On("nextEvents").Once().Return(Cursor{}, nil, assert.AnError)
+		stream.On("closeStream").Once().Return(nil)
+
+		stream.unblock <- struct{}{}
+		_, _, err := streamAPI.NextEvents()
+
+		assert.EqualError(t, assert.AnError, err.Error())
+	})
+
+	t.Run("successful read events", func(t *testing.T) {
+		stream := &mockStreamer{unblock: make(chan struct{}, 1)}
+		streamAPI, opener, _, _ := setupMockStream(nil, nil)
+
+		opener.On("openStream").Once().Return(stream, nil)
+		opener.unblock <- struct{}{}
+		stream.On("nextEvents").Twice().Return(expectedCursor, expectedEvents, nil)
+
+		stream.unblock <- struct{}{}
+		cursor, events, err := streamAPI.NextEvents()
+
+		require.NoError(t, err)
+		assert.Equal(t, expectedCursor, cursor)
+		assert.Equal(t, expectedEvents, events)
+
+		stream.unblock <- struct{}{}
+		cursor, events, err = streamAPI.NextEvents()
+
+		require.NoError(t, err)
+		assert.Equal(t, expectedCursor, cursor)
+		assert.Equal(t, expectedEvents, events)
+
+		opener.unblock <- struct{}{} // provoke an error when openStream is called
+	})
+
+	t.Run("fail with canceled", func(t *testing.T) {
+		stream := &mockStreamer{unblock: make(chan struct{}, 1)}
+		streamAPI, opener, _, _ := setupMockStream(nil, nil)
+
+		opener.On("openStream").Once().Return(stream, nil)
+		opener.unblock <- struct{}{}
+		stream.On("nextEvents").Once().Return(Cursor{}, nil, context.Canceled)
+		stream.On("closeStream").Once().Return(nil)
+
+		stream.unblock <- struct{}{}
+		_, _, err := streamAPI.NextEvents()
+
+		assert.EqualError(t, context.Canceled, err.Error())
+
+		opener.unblock <- struct{}{} // provoke an error when openStream is called
+	})
+}
+
+func TestHttpStreamAPI_CommitCursor(t *testing.T) {
+	retryCh := make(chan error, 1)
+	okCh := make(chan struct{}, 1)
+	errorCh := make(chan error, 1)
+	expectedCursor := Cursor{NakadiStreamID: "stream-id"}
+
+	t.Run("fail with time out", func(t *testing.T) {
+		streamAPI, _, committer, _ := setupMockStream(retryCh, okCh)
+		committer.On("commitCursor", expectedCursor).Twice().Return(assert.AnError)
+
+		go func() {
+			err := streamAPI.CommitCursor(expectedCursor)
+			errorCh <- err
+		}()
+
+		committer.unblock <- struct{}{}
+
+		err := <-retryCh
+		assert.EqualError(t, assert.AnError, err.Error())
+
+		time.Sleep(600 * time.Millisecond)
+		committer.unblock <- struct{}{}
+
+		err = <-errorCh
+		assert.EqualError(t, assert.AnError, err.Error())
+	})
+
+	t.Run("fail retry succeed", func(t *testing.T) {
+		streamAPI, _, committer, _ := setupMockStream(retryCh, okCh)
+		committer.On("commitCursor", expectedCursor).Once().Return(assert.AnError)
+
+		go func() {
+			err := streamAPI.CommitCursor(expectedCursor)
+			errorCh <- err
+		}()
+
+		committer.unblock <- struct{}{}
+
+		err := <-retryCh
+		assert.EqualError(t, assert.AnError, err.Error())
+
+		committer.On("commitCursor", expectedCursor).Once().Return(nil)
+		committer.unblock <- struct{}{}
+
+		err = <-errorCh
+		assert.NoError(t, err)
+
+		select {
+		case <-okCh:
+			// nothing
+		default:
+			assert.Fail(t, "no retry ok detected")
+		}
+	})
+
+	t.Run("success", func(t *testing.T) {
+		streamAPI, _, committer, _ := setupMockStream(nil, nil)
+		committer.On("commitCursor", expectedCursor).Once().Return(nil)
+
+		committer.unblock <- struct{}{}
+		err := streamAPI.CommitCursor(expectedCursor)
+
+		assert.NoError(t, err)
+	})
+}
+
+func TestHttpStreamAPI_Close(t *testing.T) {
+	errorCh := make(chan error, 1)
+	streamAPI, opener, _, _ := setupMockStream(nil, nil)
+
+	stream := &mockStreamer{unblock: make(chan struct{}, 1)}
+	opener.On("openStream").Once().Return(stream, nil)
+	stream.On("nextEvents").Once().Return(Cursor{}, nil, assert.AnError)
+	stream.On("closeStream").Once().Return(nil)
+
+	go func() {
+		err := streamAPI.Close()
+		errorCh <- err
+	}()
+
+	opener.unblock <- struct{}{}
+	time.Sleep(50 * time.Millisecond)
+	stream.unblock <- struct{}{}
+
+	err := <-errorCh
+	assert.NoError(t, err)
+	stream.AssertCalled(t, "closeStream")
+}
+
+func setupMockStream(errCh chan error, okCh chan struct{}) (StreamAPI, *mockStreamOpener, *mockCommitter, context.CancelFunc) {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	streamBackOff := backoff.NewExponentialBackOff()
+	streamBackOff.InitialInterval = 1 * time.Millisecond
+	streamBackOff.MaxInterval = 100 * time.Millisecond
+	streamBackOff.MaxElapsedTime = 500 * time.Millisecond
+
+	commitBackOff := backoff.NewExponentialBackOff()
+	commitBackOff.InitialInterval = 1 * time.Millisecond
+	commitBackOff.MaxInterval = 100 * time.Millisecond
+	commitBackOff.MaxElapsedTime = 500 * time.Millisecond
+
+	opener := &mockStreamOpener{unblock: make(chan struct{}, 1)}
+	committer := &mockCommitter{unblock: make(chan struct{}, 1)}
+
+	stream := &httpStreamAPI{
+		opener:        opener,
+		committer:     committer,
+		eventCh:       make(chan eventsOrError, 10),
+		ctx:           ctx,
+		cancel:        cancel,
+		streamBackOff: backoff.WithContext(streamBackOff, ctx),
+		commitBackOff: backoff.WithContext(commitBackOff, ctx),
+		notifyErr: func(err error, _ time.Duration) {
+			if errCh != nil {
+				errCh <- err
+			}
+		},
+		notifyOK: func() {
+			if okCh != nil {
+				okCh <- struct{}{}
+			}
+		},
+	}
+
+	go stream.startStream()
+
+	return stream, opener, committer, cancel
+}
+
+type mockStreamOpener struct {
+	mock.Mock
+	unblock chan struct{}
+}
+
+func (so *mockStreamOpener) openStream() (streamer, error) {
+	<-so.unblock
+	args := so.Called()
+	if args.Error(1) != nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(streamer), nil
+}
+
+type mockStreamer struct {
+	mock.Mock
+	unblock chan struct{}
+}
+
+func (s *mockStreamer) nextEvents() (Cursor, []byte, error) {
+	<-s.unblock
+	args := s.Called()
+	if args.Error(2) != nil {
+		return Cursor{}, nil, args.Error(2)
+	}
+	return args.Get(0).(Cursor), args.Get(1).([]byte), nil
+}
+
+func (s *mockStreamer) closeStream() error {
+	return s.Called().Error(0)
+}
+
+type mockCommitter struct {
+	mock.Mock
+	unblock chan struct{}
+}
+
+func (c *mockCommitter) commitCursor(cursor Cursor) error {
+	<-c.unblock
+	return c.Called(cursor).Error(0)
+}
