2bc6fda6a4c80de5f098151651e4edcc61208e28
Nikolai Krapivchenko <krapivchenkon@gmail.com>
2018-08-15 12:44:49 +0200

    issue-50 implemented Formatter for BatchItemsError

--- publish.go
+++ publish.go
@@ -1,10 +1,14 @@
 package nakadi
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
+	"sort"
+	"strings"
 	"time"
 
 	"github.com/pkg/errors"
@@ -163,4 +167,37 @@ type BatchItemsError []BatchItemResponse
 // Error implements the error interface for BatchItemsError.
 func (err BatchItemsError) Error() string {
 	return "one or many events may have not been published"
+}
+
+// Format implements fmt.Formatter for BatchItemsError
+func (err BatchItemsError) Format(s fmt.State, verb rune) {
+	switch verb {
+	case 'v':
+		if s.Flag('+') {
+			var messages []string
+			for k, v := range err {
+				messages = append(messages, fmt.Sprintf("[%d]: %+v", k, v))
+			}
+			sort.Strings(messages)
+
+			builder := bytes.NewBuffer(make([]byte, 0))
+			switch len(err) {
+			case 0:
+				builder.WriteString("an error occurred while publishing event")
+				return
+			case 1:
+				builder.WriteString("an error occurred while publishing event: ")
+			default:
+				builder.WriteString("errors occurred while publishing events: ")
+			}
+
+			builder.WriteString(strings.Join(messages, ", "))
+			io.WriteString(s, builder.String())
+
+			return
+		}
+		fallthrough
+	case 's', 'q':
+		io.WriteString(s, err.Error())
+	}
 }
